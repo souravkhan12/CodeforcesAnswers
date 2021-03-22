a33cc3c14e7f81ac1ef280083df805ce0698b877
Sourav khan <62439589+souravkhan12@users.noreply.github.com>
2021-03-22 11:52:43 +0530

    Create 54B-coins.cpp

---
+++ 54B-coins.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*---------------------------------------------------------------*/
+#define int long long
+#define  grub_efi() ios_base::sync_with_stdio(false),cin.tie(nullptr);
+#define rep(i,a,b) for(int i =a;i<b;++i)
+#define srt(v) sort(v.begin(),v.end())
+#define nline cout << "\n"
+/*---------------------------------------------------------------*/
+int gcd(int a,int b){return ( b==0 ? a :gcd(b,a%b));}
+/*---------------------------------------------------------------
+void seive(int *a,int n){
+    memset(a,true,sizeof(a));
+    for(int i =2 ;i<n;++i){
+        if(a[i]==true){
+            for(int p = i*i;i<n;p+=i){
+                a[p]=false;
+            }
+        }
+    }
+}
+------------------------------------------------------------------*/
+
+void solve(){
+    int N ; cin >> N;
+    cout << N << " ";
+    for(int i = N-1 ; i>= 1;i--){
+        if(N%i==0){
+            cout << i << " ";
+            N = i;
+        }
+    }
+}
+
+signed main(){
+    grub_efi();
+    int _ = 1;
+    while (_-->0)solve();
+    return 0;
+}
+
+
